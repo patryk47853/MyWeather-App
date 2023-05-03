2e7fcc3d15ddfe840198be3d2824714b88510e91
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-05-03 16:02:48 +0200

    Merge HomeController with WeatherController

--- src/main/java/pl/patrykjava/controller/WeatherController.java
+++ src/main/java/pl/patrykjava/controller/WeatherController.java
@@ -1,19 +1,27 @@
 package pl.patrykjava.controller;
 
-import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.patrykjava.model.WeatherDto;
 import pl.patrykjava.service.WeatherService;
 
-@RestController
-@RequiredArgsConstructor
+@Controller
 public class WeatherController {
 
     private final WeatherService weatherService;
 
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
     @GetMapping("/weather")
-    public WeatherDto getWeather() {
-        return weatherService.getWeather();
+    public String home(@RequestParam String city, Model model) {
+        WeatherDto weather = weatherService.getWeather(city);
+        model.addAttribute("weather", weather);
+        return "weather";
     }
 }
+
