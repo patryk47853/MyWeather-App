8ab23d1ce8c7aa02309755971c051a4ed8546e6c
Patryk Maliszewski <p.maliszewski00@gmail.com>
2023-05-05 20:30:38 +0200

    Create tests for Weather Controller

---
+++ src/test/java/pl/patrykjava/WeatherControllerTest.java
@@ -0,0 +1,68 @@
+package pl.patrykjava;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+import pl.patrykjava.controller.WeatherController;
+import pl.patrykjava.model.WeatherDto;
+import pl.patrykjava.service.WeatherService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class WeatherControllerTest {
+
+    @Mock
+    private WeatherService weatherService;
+
+    @InjectMocks
+    private WeatherController weatherController;
+
+    @Mock
+    private Model theModel;
+
+    @Test
+    public void home_ShouldReturnHomeView_WhenCityIsNull() {
+        // Arrange
+        String city = null;
+
+        // Act
+        String viewName = weatherController.home(city, theModel);
+
+        // Assert
+        assertEquals("home", viewName);
+    }
+
+    @Test
+    public void home_ShouldReturnErrorView_WhenWeatherIsNull() {
+        // Arrange
+        String city = "TestCity";
+        when(weatherService.getWeather(city)).thenReturn(null);
+
+        // Act
+        String viewName = weatherController.home(city, theModel);
+
+        // Assert
+        assertEquals("error", viewName);
+    }
+
+    @Test
+    public void home_ShouldAddWeatherToModelAndReturnWeatherView_WhenWeatherIsNotNull() {
+        // Arrange
+        String city = "Warsaw";
+        WeatherDto weather = new WeatherDto();
+        when(weatherService.getWeather(city)).thenReturn(weather);
+
+        // Act
+        String viewName = weatherController.home(city, theModel);
+
+        // Assert
+        assertEquals("weather", viewName);
+        verify(theModel).addAttribute("weather", weather);
+    }
+}
